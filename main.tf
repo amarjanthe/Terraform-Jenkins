066b523646f1462b2e06899355bdf980b9992218
amarjanthe <159176597+amarjanthe@users.noreply.github.com>
2024-11-17 17:04:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "jenkins-server" {
+resource "aws_instance" "jenkinsserver" {
   ami           = "ami-0866a3c8686eaeeba" # us-east-1
   instance_type = "t2.micro"
   tags = {
